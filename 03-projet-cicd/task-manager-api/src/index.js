819ce27d18450b288058d41f82c8cc3fd7066aac
Abdoulkarimou <abdoulkarimmahamadou4@gmail.com>
2026-07-29 10:00:03 +0100

    Delete 03-projet-cicd directory

--- 03-projet-cicd/task-manager-api/src/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-
-app.listen(3000, () => {
-  console.log('-----------------------------------');
-  console.log('=== Server running on port 3000 ===');
-  console.log('-----------------------------------');
-});
