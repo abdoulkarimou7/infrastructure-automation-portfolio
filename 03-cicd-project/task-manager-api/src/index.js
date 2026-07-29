fc168bbb9d943004eccfd2b029cddaec84772d4e
abdoulkarimou7 <abdoulkarimmahamadou4@gmail.com>
2026-07-29 10:21:28 +0100

    docs: update Hero and Skills on live portfolio

--- 03-cicd-project/task-manager-api/src/index.js
+++ 03-cicd-project/task-manager-api/src/index.js
@@ -1,7 +1,5 @@
 const app = require('./app');
 
 app.listen(3000, () => {
-  console.log('-----------------------------------');
   console.log('=== Server running on port 3000 ===');
-  console.log('-----------------------------------');
 });
